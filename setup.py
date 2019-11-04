f98ac307c6f113a4c28865080ebdafae43e321d1
Vittorio Ghirardini <50493608+vittoriodx7@users.noreply.github.com>
2019-11-04 11:57:55 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+      name='ds9_colormap',    # This is the name of your PyPI-package.
+      version='1.0',
+      description='Python package to bypass ds9',
+      author='Vittorio Ghirardini',
+      author_email='vittorio@mpe.mpg.de',
+      url="https://github.com/vittoriodx7/ds9_colormap",
+      packages=['pyproffit'],
+      install_requires=[
+            'astropy','matplotlib','os','numpy'
+      ],
+)
