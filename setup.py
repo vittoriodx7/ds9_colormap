7bb419d2b520ac107b9b6886a7a360cd58e11c34
vittoriodx7 <vercan90l19>
2019-11-08 09:09:22 +0100

    bu

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
       name='ds9_colormap',    # This is the name of your PyPI-package.
       version='1.0',
       description='Python package to bypass ds9',
-      author='Vittorio Ghirardini',
+      author='Vittorio Ghirardini,
       author_email='vittorio@mpe.mpg.de',
       url="https://github.com/vittoriodx7/ds9_colormap",
       packages=['ds9_colormap'],
