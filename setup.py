76bf63ed26e8f8576dbc0e225d527aafb8b9c9eb
vittoriodx7 <vercan90l19>
2019-11-13 16:29:36 +0100

    bug fixing

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
       name='ds9_colormap',    # This is the name of your PyPI-package.
       version='1.0',
       description='Python package to bypass ds9',
-      author='Vittorio Ghirardini,
+      author='Vittorio Ghirardini',
       author_email='vittorio@mpe.mpg.de',
       url="https://github.com/vittoriodx7/ds9_colormap",
-      packages=['ds9_colormap'],
+      packages=['ds9_colomap'],
       install_requires=[
             'astropy','matplotlib','numpy'
       ],
